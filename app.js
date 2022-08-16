1417c625b0124165369881443db3644209032aa0
jooy3410 <jooy3410@naver.com>
2022-08-16 23:45:26 +0900

    자바스크립트 공부(function)

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/study', (req,res) =>{
+    res.render('study.html')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
