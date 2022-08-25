3806380b9c1bed56d8b3d478445268e9cfbb51fc
jooy3410 <jooy3410@naver.com>
2022-08-25 23:39:32 +0900

    callback

---
+++ public/callback.js
@@ -0,0 +1,22 @@
+//JavaScript is synchronous.
+//Execute the code block by orger after goisting.
+//hoisting: var, function 선언이 맨위로 가는것
+
+//동기: 순서대로 실행되는것
+//하나끝나면 하나 실행되고 그다음것이 끝나면 그다음것이 실행된다.
+console.log('1');
+console.log('2');
+console.log('3');
+
+//비동기
+
+console.log('1');
+setTimeout(function () {
+    console.log('2');
+}, 1000);
+console.log('3');
+
+//Synchronous callback
+
+
+//Asynchronous callback
