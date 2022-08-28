9a6747ddfec4e5f0920d2c82b9dc7234c9830bb8
jooy3410 <jooy3410@naver.com>
2022-08-28 23:48:54 +0900

    .

--- public/callback.js
+++ public/callback.js
@@ -17,6 +17,10 @@ setTimeout(function () {
 console.log('3');
 
 //Synchronous callback
+function printImmediately(print) {
+    print();
+}
 
+printImmediately(()=> console.log('hello'));
 
 //Asynchronous callback
